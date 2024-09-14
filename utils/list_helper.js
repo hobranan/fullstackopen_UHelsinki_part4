5a7a2df5b5d679953ab5a686c168187293906720
Brandon Hoynick <hoynick@ualberta.ca>
2024-09-14 17:27:39 -0600

    submitting part4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  let sum = 0;
+  blogs.forEach(function (num) {
+    sum += num.likes;
+  });
+  return sum;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
