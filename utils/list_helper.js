3393ba4c1593eb7ecafb3dabaa2397509af3cd34
Brandon Hoynick <hoynick@ualberta.ca>
2024-09-14 19:05:21 -0600

    submitting part4.5, part4.6 and part47

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,16 +1,74 @@
+const logger = require("../utils/logger");
+
 const dummy = (blogs) => {
   return 1;
 };
 
 const totalLikes = (blogs) => {
   let sum = 0;
-  blogs.forEach(function (num) {
-    sum += num.likes;
+  blogs.forEach(function (entry) {
+    sum += entry.likes;
   });
+  logger.info(sum);
   return sum;
+};
+
+const favoriteBlog = (blogs) => {
+  let max = 0;
+  let fav = {};
+  blogs.forEach(function (entry) {
+    if (entry.likes > max) {
+      max = entry.likes;
+      fav = entry;
+    }
+  });
+  const temp_fav = {
+    title: fav.title,
+    author: fav.author,
+    likes: fav.likes,
+  };
+  logger.info(temp_fav);
+  return temp_fav;
+};
+
+const mostBlogs = (blog_array) => {
+  let max = 0;
+  let result = {};
+  blog_array.forEach(function (entry) {
+    if (entry.blogs > max) {
+      max = entry.blogs;
+      result = entry;
+    }
+  });
+  const final_result = {
+    author: result.author,
+    blogs: result.blogs,
+  };
+  logger.info(final_result);
+  return final_result;
+};
+
+const mostLikes = (blogs) => {
+  let max = 0;
+  let result = {};
+  blogs.forEach(function (entry) {
+    if (entry.likes > max) {
+      max = entry.likes;
+      result = entry;
+    }
+  });
+  const final_result = {
+    author: result.author,
+    likes: result.likes,
+  };
+  logger.info(final_result);
+  return final_result;
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
